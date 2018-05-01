1654a9569615a7d527c28f2cf7f476f4f9ae1f24
Kingsone <kingsone995@gmail.com>
2018-05-01 18:35:08 +0800

    first modify

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 //$im=imagecreatefromjpeg("./statics/default/img/bg_header_t.jpg");
 //imagejpeg($im);
 //imagedestroy($im);
-//phpinfo();
+phpinfo();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
